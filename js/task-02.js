692fad225fb17513316b017ccaeffaf043352f05
Pavelmd <pashanovosvit2@gmail.com>
2023-08-19 15:36:45 +0300

    task 2 done

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const list = document.querySelector("#ingredients");
+const liEl = document.createElement("li");
+
+ingredients.map((element) => {
+  const liEl = document.createElement("li");
+  liEl.textContent = element;
+  liEl.classList.add("item");
+  list.append(liEl);
+});
+
+
