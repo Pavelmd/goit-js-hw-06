7e9f83f920f441db49a136b1fedc5edd5ce2d83f
Pavelmd <pashanovosvit2@gmail.com>
2023-08-21 23:50:01 +0300

    task 5 done

--- js/task-04.js
+++ js/task-04.js
@@ -1,15 +1,19 @@
-const buttonDecrement = document.querySelector('button[data-action="decrement"]');
-const buttonIncrement = document.querySelector('button[data-action="increment"]');
-const valueEl = document.querySelector('#value');
+const buttonDecrement = document.querySelector(
+  'button[data-action="decrement"]'
+);
+const buttonIncrement = document.querySelector(
+  'button[data-action="increment"]'
+);
+const valueEl = document.querySelector("#value");
 let counterValue = 0;
 
-buttonDecrement.addEventListener('click', decrement);
-buttonIncrement.addEventListener('click', increment);
+buttonDecrement.addEventListener("click", decrement);
+buttonIncrement.addEventListener("click", increment);
 function decrement() {
-    counterValue -= 1;
-    valueEl.textContent = counterValue;
+  counterValue -= 1;
+  valueEl.textContent = counterValue;
 }
 function increment() {
-    counterValue += 1;
-    valueEl.textContent = counterValue;
+  counterValue += 1;
+  valueEl.textContent = counterValue;
 }
