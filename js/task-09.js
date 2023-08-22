709d3d7cce033ea0f13fd213639aab9553b39b8b
Pavelmd <pashanovosvit2@gmail.com>
2023-08-22 14:31:07 +0300

    task 9 done

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const backGround = document.querySelector(".color");
+const btnChangeColor = document.querySelector(".change-color");
+
+btnChangeColor.addEventListener("click", color);
+function color() {
+  document.body.style.backgroundColor = getRandomHexColor();
+  backGround.textContent = getRandomHexColor();
+}
